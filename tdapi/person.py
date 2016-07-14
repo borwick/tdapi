9698b848b38ff7084ff1e50ec888b1061c8999cc
John Borwick <borwick@vt.edu>
2016-07-14 11:44:32 -0700

    Activate, deactivate, update, add applications

--- tdapi/person.py
+++ tdapi/person.py
@@ -50,4 +50,39 @@ class TDPerson(tdapi.obj.TDObject):
         tdapi.TD_CONNECTION.request(method='put',
                                     url_stem=add_group_uri)
 
+    def del_group_by_id(self, group_id):
+        del_group_uri = 'people/{}/groups/{}'.format(self.get('UID'), group_id)
+        tdapi.TD_CONNECTION.request(method='delete',
+                                    url_stem=del_group_uri)
+
+    def set_active(self, active):
+        activate_uri = 'people/{}/isactive?status={}'.format(self.get('UID'), active)
+        tdapi.TD_CONNECTION.request(method='put',
+                                    url_stem=activate_uri)
+
+    def activate(self):
+        return self.set_active(True)
+
+    def deactivate(self):
+        return self.set_active(False)
+
+    def update(self, update_data):
+        update_uri = 'people/{}'.format(self.get('UID'))
+
+        # don't mess with the original data. copy into the update all
+        # existing data. TODO consider purging cache and re-calling
+        # query before doing this update.
+        update_data = copy.deepcopy(update_data)
+        for orig_attr in self.td_struct.keys():
+            if orig_attr not in update_data:
+                update_data[orig_attr] = self.td_struct[orig_attr]
+
+        tdapi.TD_CONNECTION.request(method='post',
+                                    url_stem=update_uri,
+                                    data=update_data)
+
+    def add_applications(self, app_list):
+        all_apps = list(set(self.td_struct['Applications'] + app_list))
+        return self.update({'Applications': all_apps})
+
 tdapi.obj.relate_cls_to_manager(TDPerson, TDPersonManager)
