18eec059197fda1061e0bff1dfbe64bf097c2291
John Borwick <borwick@vt.edu>
2016-07-15 10:44:47 -0700

    single_query_get() option to get all data

--- tdapi/person.py
+++ tdapi/person.py
@@ -38,25 +38,50 @@ class TDPersonManager(tdapi.obj.TDObjectManager):
 
 
 class TDPerson(tdapi.obj.TDObject):
+    def __init__(self, *args, **kwargs):
+        super(TDPerson, self).__init__(*args, **kwargs)
+        self._single_queried = False
+        
     def __str__(self):
         return self.get('FullName')
+
+    def person_id(self):
+        return self.get('UID')
+
+    def person_url(self):
+        return 'people/{}'.format(self.person_id())
+
+    def single_query_get(self, attr):
+        # modeled off off TDAsset.single_query_get
+        cached_attr_val = self.get(attr)
+        if cached_attr_val:
+            return cached_attr_val
+
+        if self._single_queried is False:
+            self.td_struct = tdapi.TD_CONNECTION.json_request(
+                method='get',
+                url_stem=self.person_url()
+                )
+            self._single_queried = True
+
+        return self.get(attr)
 
     def import_string(self):
         return '{} <{}>'.format(self.get('FullName').encode('utf-8'), self.get('AlertEmail'))
 
     def add_group_by_id(self, group_id):
         # does not currently support the optional arguments
-        add_group_uri = 'people/{}/groups/{}'.format(self.get('UID'), group_id)
+        add_group_uri = self.person_url() + '/groups/{}'.format(group_id)
         tdapi.TD_CONNECTION.request(method='put',
                                     url_stem=add_group_uri)
 
     def del_group_by_id(self, group_id):
-        del_group_uri = 'people/{}/groups/{}'.format(self.get('UID'), group_id)
+        del_group_uri = self.person_url() + '/groups/{}'.format(group_id)
         tdapi.TD_CONNECTION.request(method='delete',
                                     url_stem=del_group_uri)
 
     def set_active(self, active):
-        activate_uri = 'people/{}/isactive?status={}'.format(self.get('UID'), active)
+        activate_uri =  self.person_url() + '/isactive?status={}'.format(active)
         tdapi.TD_CONNECTION.request(method='put',
                                     url_stem=activate_uri)
 
@@ -67,8 +92,6 @@ class TDPerson(tdapi.obj.TDObject):
         return self.set_active(False)
 
     def update(self, update_data):
-        update_uri = 'people/{}'.format(self.get('UID'))
-
         # don't mess with the original data. copy into the update all
         # existing data. TODO consider purging cache and re-calling
         # query before doing this update.
@@ -78,7 +101,7 @@ class TDPerson(tdapi.obj.TDObject):
                 update_data[orig_attr] = self.td_struct[orig_attr]
 
         tdapi.TD_CONNECTION.request(method='post',
-                                    url_stem=update_uri,
+                                    url_stem=self.person_url(),
                                     data=update_data)
 
     def add_applications(self, app_list):
